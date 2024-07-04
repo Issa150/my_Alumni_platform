48345c4dab4d9870939f94ab9c1b97f918c07be4
Gwladys <jacobin.gw@hotmail.fr>
2024-07-04 12:28:41 +0200

    base accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+        // $user= true;
+        $user= false;
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'user'=> $user,
         ]);
     }
 }

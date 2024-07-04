280fccd09c13eb5911390c95de077dc18affbdbe
sophie <ostermeyer75@gmail.com>
2024-07-04 16:29:19 +0200

    easyadmin formation et emploi

---
+++ src/Entity/Formation.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FormationRepository::class)]
+class Formation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $zipcode = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $city = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $begin_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $end_at = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $degree = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $funding = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $teleworking = null;
+
+    #[ORM\ManyToOne(inversedBy: 'formations')]
+    private ?User $user_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(?string $zipcode): static
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getBeginAt(): ?\DateTimeImmutable
+    {
+        return $this->begin_at;
+    }
+
+    public function setBeginAt(?\DateTimeImmutable $begin_at): static
+    {
+        $this->begin_at = $begin_at;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->end_at;
+    }
+
+    public function setEndAt(?\DateTimeImmutable $end_at): static
+    {
+        $this->end_at = $end_at;
+
+        return $this;
+    }
+
+    public function getDegree(): ?string
+    {
+        return $this->degree;
+    }
+
+    public function setDegree(?string $degree): static
+    {
+        $this->degree = $degree;
+
+        return $this;
+    }
+
+    public function getFunding(): ?string
+    {
+        return $this->funding;
+    }
+
+    public function setFunding(?string $funding): static
+    {
+        $this->funding = $funding;
+
+        return $this;
+    }
+
+    public function isTeleworking(): ?bool
+    {
+        return $this->teleworking;
+    }
+
+    public function setTeleworking(?bool $teleworking): static
+    {
+        $this->teleworking = $teleworking;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
